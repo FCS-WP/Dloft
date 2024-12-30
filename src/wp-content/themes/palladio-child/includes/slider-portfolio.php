a0828a24aaf0cbcb216b022661745f7aa18e85ae
toanpham2111 <toan444666@gmail.com>
2024-12-30 14:08:50 +0700

    update profolio-slider

--- src/wp-content/themes/palladio-child/includes/slider-portfolio.php
+++ src/wp-content/themes/palladio-child/includes/slider-portfolio.php
@@ -5,6 +5,10 @@ function category_posts_slider_shortcode($atts)
     wp_enqueue_style('slick-theme-css', THEME_URL . '-child' . '/assets/lib/slick/slick-theme.css');
     wp_enqueue_script('slick-slider-js', THEME_URL . '-child' . '/assets/lib/slick/slick.min.js', array('jquery'), null, true);
 
+    // Enqueue FancyBox
+    wp_enqueue_style('fancybox-css', THEME_URL . '-child' . '/assets/lib/fancybox/jquery.fancybox.min.css');
+    wp_enqueue_script('fancybox-js', THEME_URL . '-child' . '/assets/lib/fancybox/jquery.fancybox.min.js', array('jquery'), null, true);
+
     wp_add_inline_script('slick-slider-js', "
     jQuery(document).ready(function($) {
         $('.category-slider').slick({
@@ -55,7 +59,16 @@ function category_posts_slider_shortcode($atts)
             
             $('.category-column[data-category-id=\"' + currentCategory + '\"]').addClass('slick-current');
         });
-    
+
+        // Initialize FancyBox
+        $('[data-fancybox]').fancybox({
+            buttons: [
+                'zoom',
+                'close',
+                'slideShow',
+                'fullScreen',
+            ]
+        });
     });
     ");
 
@@ -68,6 +81,17 @@ function category_posts_slider_shortcode($atts)
     if (empty($categories)) {
         return '<p>No categories found.</p>';
     }
+
+    $desired_order = array(58, 60, 64, 61, 71, 62, 63, 70); 
+    usort($categories, function ($a, $b) use ($desired_order) {
+        $pos_a = array_search($a->term_id, $desired_order);
+        $pos_b = array_search($b->term_id, $desired_order);
+
+        $pos_a = $pos_a === false ? PHP_INT_MAX : $pos_a;
+        $pos_b = $pos_b === false ? PHP_INT_MAX : $pos_b;
+
+        return $pos_a - $pos_b;
+    });
 
     ob_start();
 ?>
@@ -108,9 +132,8 @@ function category_posts_slider_shortcode($atts)
                     ?>
                             <li class="post-item">
                                 <div class="post-thumb-wrap">
-                                    <a class="post-thumb-link">
+                                    <a class="post-thumb-link" data-fancybox="gallery" href="<?php echo esc_url($thumb_url); ?> ">
                                         <img src="<?php echo esc_url($thumb_url); ?>" alt="<?php echo esc_attr(get_the_title($post->ID)); ?>" class="post-thumb">
-                                        <span class="post-title"><?php echo esc_html(get_the_title($post->ID)); ?></span>
                                     </a>
                                 </div>
                             </li>
@@ -129,4 +152,29 @@ function category_posts_slider_shortcode($atts)
 }
 
 add_shortcode('category_posts_slider', 'category_posts_slider_shortcode');
+
+// Register the shortcode
+function custom_boxes_shortcode() {
+    return '
+    <div class="custom-box-container">
+        <a href="https://www.namaik.com/our-valuable-team/" target="_blank">
+            <div class="custom-box img">
+                <img src="/wp-content/uploads/2024/12/Screenshot-from-2024-12-30-11-02-40.png" alt="Our Partners">
+                <span>Our Partners</span>
+            </div>
+        </a>
+        <a href="https://www.3pecotecture.com/contact_staff.html" target="_blank">
+            <div class="custom-box img">
+                <img src="/wp-content/uploads/2024/12/images.jpeg" alt="Affiliated Firm">
+                <span>Affiliated Firm</span>
+            </div>
+        </a>
+        <a href="https://scholarbank.nus.edu.sg/handle/10635/242199" target="_blank">
+            <div class="custom-box">
+                <span>Thesis</span>
+            </div>
+        </a>
+    </div>';
+}
+add_shortcode('custom_boxes', 'custom_boxes_shortcode');
 ?>
